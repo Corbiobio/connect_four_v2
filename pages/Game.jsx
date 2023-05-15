496f60a55cf740b57c93ccd9594404bc9fede49d
Corbiobio <edarnand@edenschool.fr>
2023-05-15 19:37:58 +0200

    test react

---
+++ pages/Game.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom"
+
+function Game() {
+    return (
+        <section>
+            <h1>Game page</h1>
+        </section>
+    )
+}
+
+export default Game
