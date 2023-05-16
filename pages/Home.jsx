b048e08a7d99f5c29e94c54c0d8991a386b6ed5b
corbiobio <edarnand@edenschool.fr>
2023-05-16 17:16:50 +0200

    button on home page add

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function App() {
     return (
         <main>
-            <h1>Home</h1>
+            <nav>
+                <img src="/images/logo.svg" alt="logo" />
+                <div>
+                    <button><Link to="/game" >Payer vs PLayer </Link><img src="./player-vs-player.svg" alt="player vs player" /></button>
+                    <button><Link to="/rules">Game rules</Link></button>
+                </div>
+            </nav>
         </main>
     )
 }
