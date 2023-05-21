d18350e8ef0bb34e384ac2a22bb26a8ef7d44a9c
Corbiobio <edarnand@edenschool.fr>
2023-05-21 20:04:49 +0200

    nav bar add on game page

--- pages/Game.jsx
+++ pages/Game.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Game() {
     return (
-        <div>
-            <h1>Game</h1>
-        </div>
+        <main id='Game'>
+            <nav>
+                <button className='btn_nav'>menu</button>
+                <img src="/images/logo.svg" alt="logo" />
+                <a href="/game" className='btn_nav'>restart</a>
+            </nav>
+        </main >
     )
 }
