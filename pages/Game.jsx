6d01b6309fdedcd7001ee2b95864fef59c34c8e1
corbiobio <edarnand@edenschool.fr>
2023-05-22 17:16:17 +0200

    nav bar , div player, div board, bottom

--- pages/Game.jsx
+++ pages/Game.jsx
@@ -8,6 +8,84 @@ export default function Game() {
                 <img src="/images/logo.svg" alt="logo" />
                 <a href="/game" className='btn_nav'>restart</a>
             </nav>
+            <div className='game'>
+                <div className='player_one player'>
+                    <p>player 1</p>
+                    <strong>0</strong>
+                    <img src="/images/player-one.svg" alt="smiling face" />
+                </div>
+
+                <div className='player_two player'>
+                    <p>player 2</p>
+                    <strong>0</strong>
+                    <img src="/images/player-two.svg" alt="smiling face" />
+                </div>
+                <div className='board'>
+                    <img src="/images/board-layer-white-small.svg" alt="board" />
+                    <img src="/images/board-layer-black-small.svg" alt="board" />
+                    <div className='board_game'>
+                        <div className='column'>
+                            <div className="item"><img src="/images/counter-yellow-small.svg" alt="" /></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                        </div>
+                        <div className='column'>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                        </div>
+                        <div className='column'>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                        </div>
+                        <div className='column'>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                        </div>
+                        <div className='column'>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                        </div>
+                        <div className='column'>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                        </div>
+                        <div className='column'>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                            <div className="item"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='bottom'>
+
+            </div>
         </main >
     )
 }
